63f9e4f336842ba4a9944524da22f8293a1be3f6
estcr <est_cr@hotmail.com>
2024-11-21 16:42:25 +0100

    path fix

--- backend.py
+++ backend.py
@@ -4,7 +4,7 @@ import graficos as g
 
 
 def load_data():
-    df=pd.read_csv(r"/zalando\data\datos_aleatorios_2023.csv")
+    df=pd.read_csv(r"https://raw.githubusercontent.com/estcr/lab-3er-semana/refs/heads/main/zalando/data/datos_aleatorios_2023.csv")
     df=funcion_principal(df)
     return df
 
